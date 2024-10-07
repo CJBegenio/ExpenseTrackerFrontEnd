c7f506f7ab2a36038aa6ea3b5a0fab01ff67e142
Charles Begenio <begenioc@gmail.com>
2024-10-07 12:47:54 +0800

    Design edited, responsive design/bootsrap WIP

--- script.js
+++ script.js
@@ -12,26 +12,3 @@ loginBtn.addEventListener('click', () => {
 
 
 // dashboard //
-
-function navigateToPage(elementId, url) {
-    const element = document.getElementById(elementId);
-    
-    if (!element) {
-        console.error(`Element with ID '${elementId}' not found.`);
-        return;
-    }
-
-    element.addEventListener('click', function() {
-        if (!url || typeof url !== 'string') {
-            console.error(`Invalid URL provided for the ${elementId} element.`);
-            return;
-        }
-        
-        window.location.href = url;
-    });
-}
-navigateToPage('profile-page', 'profile.html');
-navigateToPage('expenses-page', 'expenses.html');
-navigateToPage('income-page', 'income.html');
-navigateToPage('settings-page', 'settings.html');
-navigateToPage('faqs-page', 'faqs.html');
